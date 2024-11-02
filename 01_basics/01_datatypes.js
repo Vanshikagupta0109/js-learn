104336866b89287aa6a499a7c743eb9e54599bc6
Vanshikagupta0109 <147362803+Vanshikagupta0109@users.noreply.github.com>
2024-11-02 10:08:27 +0000

    container config done

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,11 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+2) we are using node js not browser
+
+let name="vanshika"//string
+let age=18//number
+let isloggedin= false //boolean
+
+console.log(typeof null );//object
+console.log(typeof undefined );//undefined
+
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "vanshika.com"
+var accountpassword="234455"
+accountcity = "ayodhya"
+
+//accountId = 2 not allowed
+
+console.log(accountId);
+
+accountEmail="vanshii.com"
+accountpassword="1221"
+accountcity ="delhi"
+
+console.table([accountId,accountEmail, accountpassword, accountcity]);
