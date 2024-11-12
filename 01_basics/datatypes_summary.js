0cab93cd43b72599cb59c6a527242abb83009d71
Vanshikagupta0109 <147362803+Vanshikagupta0109@users.noreply.github.com>
2024-11-12 08:02:16 +0000

    container config done

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,17 @@
+//primitive
+
+
+// 7 types : string , number, boolean , null, undefined, symbol,BigInt
+const score=100
+
+const id= symbol('123')
+const anotherid=symbol('123')// these both are not same 
+
+console.log(id===anotherid);
+
+const bigNumber= 234321235456654n
+
+
+// reference(  non primitive)
+
+//array, objects, functions
