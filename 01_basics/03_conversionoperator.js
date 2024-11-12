287f57c05b082d5c308fb103bd56120bad3f8730
Vanshikagupta0109 <147362803+Vanshikagupta0109@users.noreply.github.com>
2024-11-12 06:47:20 +0000

    container config done

--- 01_basics/03_conversionoperator.js
+++ 01_basics/03_conversionoperator.js
@@ -28,3 +28,25 @@ let value = 3;
 let negvalue= -value;
 console.log(negvalue);
 
+let str1= "hello"
+let str2= "hoitesh"
+
+let str3= str1+ str2
+console.log(str3); 
+
+console.log(+true);
+console.log(+"");
+
+let num1,num2,num3 
+num1= num2=num3=2+2;
+
+let gamecounter= 100
+gamecounter++;
+console.log(gamecounter);
+
+// link to learn prefix and postfix
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+
+
+
---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,16 @@
+console.log("2">1);
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined ==0);
+console.log(undefined>0);
+
+// strict check === data type is copaed to string in example
+
+console.log("2"===2);
+
+
+
+
+
