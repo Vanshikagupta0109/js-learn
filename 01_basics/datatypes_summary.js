ba5e9911fb26b0a5374f080fd4f2093a1e2cbc6a
Vanshikagupta0109 <147362803+Vanshikagupta0109@users.noreply.github.com>
2025-02-05 15:17:22 +0000

    container config done

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -4,8 +4,8 @@
 // 7 types : string , number, boolean , null, undefined, symbol,BigInt
 const score=100
 
-const id= symbol('123')
-const anotherid=symbol('123')// these both are not same 
+const id= Symbol('123')
+const anotherid=Symbol('123')// these both are not same 
 
 console.log(id===anotherid);
 
@@ -15,3 +15,30 @@ const bigNumber= 234321235456654n
 // reference(  non primitive)
 
 //array, objects, functions
+
+
+
+
+
+//..........................
+
+// stack memeory(primitive), heap memory(non-primitive)
+
+let myyoutubename ="vanshikadotcom"
+let anothername= "vanshiii"
+anothername="chaiorcode"
+console.log(myyoutubename);
+console.log(anothername);
+
+let userone={
+    email:"user@google.com",
+    upi:"tryeu3"
+
+}
+
+let usertwo=userone
+usertwo.email="vanshka@googleemail";
+
+console.log(userone.email);
+console.log(usertwo.email);
+
