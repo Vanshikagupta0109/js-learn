6224c81cc4a50fe6251ab24f7111db5f2a09b22b
Vanshikagupta0109 <147362803+Vanshikagupta0109@users.noreply.github.com>
2025-03-30 10:15:01 +0000

    container config done

---
+++ 01_basics/08_date.js
@@ -0,0 +1,28 @@
+let myDate= new Date()
+//console.log(myDate.toDateString());
+//nconsole.log(myDate.toString());
+
+//let mycreateddate = new Date(2023,0,23)
+//console.log(mycreateddate.toDateString());
+let mycreateddate = new Date(2023,0,23,5,3)
+//console.log(mycreateddate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+//console.log(myTimeStamp);
+//console.log(mycreateddate.getTime());
+//console.log(Math.floor(Date.now()/1000));
+
+let newdate = new Date
+
+newdate.toLocaleString('default',{
+    weekday:"long",
+    timeZone:setInterval
+})
+
+
+
+
+
+
+
